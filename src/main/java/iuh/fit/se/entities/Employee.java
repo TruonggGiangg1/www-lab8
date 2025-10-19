5b7f3b94dea5b9fd2bffdd14f02796947e88c9a6
H82M19\Student <Student>
2025-10-19 14:34:41 +0700

    first

---
+++ src/main/java/iuh/fit/se/entities/Employee.java
@@ -0,0 +1,25 @@
+package iuh.fit.se.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty(message = "First Name khong duoc phep rong")
+    private String firstName;
+
+    @NotEmpty(message = "Last Name khong duoc phep rong")
+    private String lastName;
+}
